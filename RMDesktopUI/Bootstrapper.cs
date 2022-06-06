d3a941e6bb5f93185de5aef15781a7b6444796e8
Sergio Silva <sergiobtos@hotmail.com>
2022-06-06 14:12:30 -0700

    Add UI with MVVM Architecture

---
+++ RMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using RMDesktopUI.ViewModels;
+using System.Windows;
+
+namespace RMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
